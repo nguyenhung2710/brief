fbb4f0ba8bdeb5eb2c7615ec8eaf84c161446f0d
nguyenhung2710 <72492981+nguyenhung2710@users.noreply.github.com>
2021-01-18 16:47:53 +0700

    ver 6

---
+++ assets/js/dist/index.dev.js
@@ -0,0 +1,26 @@
+"use strict";
+
+$(document).ready(function () {
+  // Product
+  $('.carousel').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    asNavFor: '.carousel-thumb'
+  });
+  $('.carousel-thumb').slick({
+    slidesToShow: 5,
+    //slidesToScroll: 3,
+    asNavFor: '.carousel',
+    //dots: true,
+    centerMode: false,
+    focusOnSelect: true
+  }); // Slider main
+
+  $('.carousel__item-main').slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+});
